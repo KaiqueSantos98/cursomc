aa18140a794828c8d7db416e374e23252dc9d973
Kaique Santos <kaique_ssantos@outlook.com>
2019-12-13 23:19:12 -0300

    Projeto criado

---
+++ src/test/java/com/kaiquecosta/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaiquecosta.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
