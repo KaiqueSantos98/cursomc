8022035e7ea3f8474486da8e92160305082e8a31
Kaique Santos <kaique_ssantos@outlook.com>
2019-12-20 22:28:51 -0300

    Populando banco de dados ao iniciar App Spring

--- src/main/java/com/kaiquecosta/cursomc/CursomcApplication.java
+++ src/main/java/com/kaiquecosta/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.kaiquecosta.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.kaiquecosta.cursomc.domain.Categoria;
+import com.kaiquecosta.cursomc.repositores.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
 	}
 
 }
