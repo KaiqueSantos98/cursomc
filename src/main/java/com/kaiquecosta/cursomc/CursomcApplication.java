a8188bb62f1b687ef42afe0e74a78c1b62b5b899
Kaique Santos <kaique_ssantos@outlook.com>
2019-12-23 12:05:04 -0300

    Entity Estados e Cidades

--- src/main/java/com/kaiquecosta/cursomc/CursomcApplication.java
+++ src/main/java/com/kaiquecosta/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kaiquecosta.cursomc.domain.Categoria;
+import com.kaiquecosta.cursomc.domain.Cidade;
+import com.kaiquecosta.cursomc.domain.Estado;
 import com.kaiquecosta.cursomc.domain.Produto;
 import com.kaiquecosta.cursomc.repositores.CategoriaRepository;
+import com.kaiquecosta.cursomc.repositores.CidadeRepository;
+import com.kaiquecosta.cursomc.repositores.EstadoRepository;
 import com.kaiquecosta.cursomc.repositores.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,7 +24,13 @@ public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 
+	@Autowired
+	private CidadeRepository cidadeRepository;	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -57,6 +67,23 @@ public class CursomcApplication implements CommandLineRunner{
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
 		
+		/*
+		 * Populando Estados e Cidades
+		 */	
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 
 }
